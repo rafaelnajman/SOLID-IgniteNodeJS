0ddd9117514c11d97b3e8d4df5dfa1cf1fa229eb
rafaelnajman <rafael.najman@hotmail.com>
2022-03-23 22:48:32 +0200

    finished challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,23 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+        if (!user) throw new Error("user not found");
+        else if (user.admin === false)
+            throw new Error(
+                "Non admin user should not be able to list all users"
+            );
+        else {
+            return this.usersRepository.list();
+        }
+    }
 }
 
 export { ListAllUsersUseCase };
